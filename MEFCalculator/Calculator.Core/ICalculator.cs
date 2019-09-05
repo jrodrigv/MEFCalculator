d1c3c53469ddc3cf891f6ee1d5953748ed2ede11
Michel Pinto <michel.pinto@roche.com>
2019-09-05 15:32:25 +0200

    Added some examples of the new features of C# 8.0

--- MEFCalculator/Calculator.Core/ICalculator.cs
+++ MEFCalculator/Calculator.Core/ICalculator.cs
@@ -1,7 +1,11 @@
-﻿namespace Calculator.Core
+﻿using System.Threading.Tasks;
+
+namespace Calculator.Core
 {
     public interface ICalculator
     {
         string Calculate(string input);
+
+        Task<string> AvailableOperationsAsync();
     }
 }
--- MEFCalculator/Calculator.Core/IOperation.cs
+++ MEFCalculator/Calculator.Core/IOperation.cs
@@ -3,5 +3,8 @@
     public interface IOperation
     {
         int Operate(int left, int right);
+
+        // C# 8.0 feature: Default interface members and Enhancement of interpolated verbatim strings
+        public string PrittyToString() => @$"Operator: {GetType().Name}";
     }
 }
